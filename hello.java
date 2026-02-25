9bd4e871b71151c4b8c0bb3ec2097d81295987a0
Aniket-Khaire11 <khaireaniket11@gmail.com>
2026-02-25 11:45:19 +0530

    Fix syntax for printing in HelloWorld class

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.("Hello, Aniiiiiketttttt!");
+        System.out.println("Hello, Aniiiiiketttttt!");
     }
 
 }
+
 
 
 
