bd9594ffc50d910aafd4c23271764e1dc551773b
Aniket-Khaire11 <khaireaniket11@gmail.com>
2026-02-25 11:43:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Aniiiiiketttttt!");
+        System.out.("Hello, Aniiiiiketttttt!");
     }
 
 }
+
 
 
